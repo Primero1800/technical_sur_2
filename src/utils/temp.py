c7c5bc7267057ce9120ceea2aa66ec52897035cb
Primero1800 <Primero@inbox.ru>
2025-02-27 15:33:10 +0300

    refactoring(Response) APP_422_CODE_STATUS added

--- src/utils/temp.py
+++ src/utils/temp.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from functools import partial
+from typing import Any, List
+
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi.exceptions import RequestValidationError
 from starlette import status
 from starlette.requests import Request
@@ -6,6 +9,44 @@ from starlette.responses import JSONResponse
 
 
 router = APIRouter(prefix='/temp')
+
+
+class GetDataFromHeader:
+    def __init__(
+        self,
+        param_names: List[str] = ['data',],
+        get_all: bool = False
+    ):
+        self.param_names = param_names.copy()
+        self.get_all = get_all
+
+    def __call__(self, request: Request) -> dict:
+        result = {}
+        for param in self.param_names:
+            if param not in request.headers:
+                if self.get_all:
+                    raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        detail=f"No {param} in request"
+                    )
+                else:
+                    continue
+            result[param] = request.headers[param]
+        return result
+
+
+@router.get("/head")
+# @router.get("head/{id}")
+async def temp(
+        data: dict | None = Depends(
+            GetDataFromHeader(['value', 's'], False)
+        )):
+    if not data:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"No valid data in request"
+        )
+    return data
 
 
 @router.get("")
