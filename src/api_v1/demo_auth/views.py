919373c54bb1e43be700ba6e158b92034ae65746
Primero1800 <Primero@inbox.ru>
2025-03-04 14:42:24 +0300

    feat(auth) demo auth added

--- src/api_v1/demo_auth/views.py
+++ src/api_v1/demo_auth/views.py
@@ -1,9 +1,12 @@
 import secrets
-from typing import Annotated
+import uuid
+from datetime import datetime
+from typing import Annotated, Any
 
-from fastapi import APIRouter, Depends, HTTPException, Header
+from fastapi import APIRouter, Depends, HTTPException, Header, Response, Cookie
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette import status
+from starlette.requests import Request
 
 router = APIRouter()
 security = HTTPBasic()
@@ -45,11 +48,13 @@ async def bad_request(
 @router.get('/basic-auth')
 async def basic_auth_credentials(
         # credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+        request: Request,
         credentials: HTTPBasicCredentials = Depends(security)
 ):
     return {
         'message': f"Hi {credentials.username}!",
-        'detail': f"Your password is '{credentials.password}'. Keep it in secret..."
+        'detail': f"Your password is '{credentials.password}'. Keep it in secret...",
+        # 'request': request.headers
     }
 
 
@@ -68,11 +73,13 @@ async def get_auth_user_username(credentials: HTTPBasicCredentials = Depends(sec
 @router.get('/basic-auth-username')
 async def basic_auth_username(
         # credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+        request: Request,
         auth_username: str = Depends(get_auth_user_username)
 ):
     return {
         'message': f"Hi {auth_username}!",
-        'detail': f"Your already logged in"
+        'detail': f"Your already logged in",
+        # 'request': request.headers
     }
 
 
@@ -96,5 +103,70 @@ async def demo_auth_some_http_header(
         'message': f"Hi {username}!",
         'detail': f"Your already logged in"
     }
+
+COOKIES: dict[str, dict[str, Any]] = {}
+COOKIE_SESSION_ID = 'web-app-cookie-session-id'
+
+
+async def generate_session_id() -> str:
+    return uuid.uuid4().hex
+
+
+@router.post('/login-cookie')
+async def demo_auth_login_cookie(
+        response: Response,
+        auth_username: str = Depends(get_auth_user_username)
+):
+    session_id = await generate_session_id()
+    COOKIES[session_id] = {
+        'cooked_username': auth_username,
+        'login_at': datetime.now()
+    }
+    response.set_cookie(COOKIE_SESSION_ID, session_id)
+    return {'good': 'boy'}
+
+
+CustomizedCookie = Cookie(alias=COOKIE_SESSION_ID, default=None, include_in_schema=False)
+
+
+async def get_session_data(
+        session_id: str = CustomizedCookie
+) -> dict:
+    if session_data := COOKIES.get(session_id):
+        return session_data
+    await unauthed(
+        detail='Not authenticated, no session_data',
+        auth_headers=None,
+    )
+
+
+@router.get('/check-login-by-cookie')
+async def check_login_by_cookie(
+    user_session_data: dict = Depends(get_session_data)
+):
+    return {
+        'result': 'You got session data from cookies',
+        **user_session_data,
+    }
+
+
+@router.get('/logout_cookie')
+async def logout_cookie(
+        response: Response,
+        session_id: str = CustomizedCookie,
+        user_data: dict = Depends(get_session_data)
+):
+    try:
+        username: dict = user_data['cooked_username']
+    except KeyError:
+        await bad_request(detail='Invalid user data')
+
+    response.delete_cookie(COOKIE_SESSION_ID)
+    # COOKIES.pop(session_id)
+    del COOKIES[session_id]
+    return {
+        'message': f"User '{username}' successfully logged out"
+    }
+
 
 
